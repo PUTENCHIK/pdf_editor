702de5066f008d4e1b61a5222b611d0073e06fe4
Astrof123 <valeev_101@bk.ru>
2024-12-04 13:12:58 +0800

    Added server requests for all functions

--- pdf_editor.client/src/pages/DeletePagePage/DeletePagePage.jsx
+++ pdf_editor.client/src/pages/DeletePagePage/DeletePagePage.jsx
@@ -34,7 +34,12 @@ function DeletePagePage() {
 
                     <form method='POST' action="api/PDF/DeletePage" onSubmit={handlerSubmit}>
                         <input type="file" name="filepdf" className='filepdf' id="" />
+                        <input type="file" name="filepdf" className='filepdf2' id="" />
                         <input type='number' name='pageNumber' className='pageNumber' placeholder="Номер страницы"/>
+                        <input type='number' name='pageNumber' className='x' placeholder="x"/>
+                        <input type='number' name='pageNumber' className='y' placeholder="y"/>
+                        <input type='number' name='pageNumber' className='width' placeholder="Ширина"/>
+                        <input type='number' name='pageNumber' className='height' placeholder="Высота"/>
                         <button type="submit">Отправить</button>
                     </form>
 
@@ -55,14 +60,19 @@ function DeletePagePage() {
         event.preventDefault();
 
         const fileInput = event.target.querySelector('.filepdf');
-        const pageInput = event.target.querySelector('.pageNumber');
+        const fileInput2 = event.target.querySelector('.filepdf2');
+        const pageNumber = event.target.querySelector('.pageNumber');
+        const x = event.target.querySelector('.x');
+        const y = event.target.querySelector('.y');
+        const width = event.target.querySelector('.width');
+        const height = event.target.querySelector('.height');
+
 
         const file = fileInput.files[0];
-        const pageNumber = pageInput.value;
-        console.log(pageNumber);
+        const file2 = fileInput2.files[0];
 
         if (file) {
-            const result = await deletePageRequest(file, pageNumber);
+            const result = await combinePdfFilesRequest(file, file2);
             if (result){
                 const blob = await result.blob();
 
@@ -89,6 +99,189 @@ function DeletePagePage() {
 
         try {
             const response = await fetch('https://localhost:7199/api/PDF/DeletePage', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData?.message || `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+
+            const data = await response;
+            console.log('Success:', data);
+            return data;
+        } catch (error) {
+            console.error('Error uploading:', error);
+            return null;
+        }
+    }
+
+
+    async function swapPagesRequest(file, pageFrom, pageTo) {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('pageFrom', pageFrom);
+        formData.append('pageTo', pageTo);
+
+        try {
+            const response = await fetch('https://localhost:7199/api/PDF/SwapPages', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData?.message || `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+
+            const data = await response;
+            console.log('Success:', data);
+            return data;
+        } catch (error) {
+            console.error('Error uploading:', error);
+            return null;
+        }
+    }
+
+
+    async function insertImageRequest(file, imageFile, pageNumber, width, height, x, y) {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('imageFile', imageFile);
+        formData.append('pageNumber', pageNumber);
+        formData.append('width', width);
+        formData.append('height', height);
+        formData.append('x', x);
+        formData.append('y', y);
+        
+
+        try {
+            const response = await fetch('https://localhost:7199/api/PDF/InsertImage', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData?.message || `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+
+            const data = await response;
+            console.log('Success:', data);
+            return data;
+        } catch (error) {
+            console.error('Error uploading:', error);
+            return null;
+        }
+    }
+
+
+    async function rotatePagesRequest(file, degrees) {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('degrees', degrees);
+        
+
+        try {
+            const response = await fetch('https://localhost:7199/api/PDF/RotatePages', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData?.message || `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+
+            const data = await response;
+            console.log('Success:', data);
+            return data;
+        } catch (error) {
+            console.error('Error uploading:', error);
+            return null;
+        }
+    }
+
+
+    async function addTextRequest(file, text, pageNumber, x, y, fontSize, font, isBold, fontColor) {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('text', text);
+        formData.append('pageNumber', pageNumber);
+        formData.append('x', x);
+        formData.append('y', y);
+        formData.append('fontSize', fontSize);
+        formData.append('font', font);
+        formData.append('isBold', isBold);
+        formData.append('fontColor', fontColor);
+        
+
+        try {
+            const response = await fetch('https://localhost:7199/api/PDF/AddText', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData?.message || `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+
+            const data = await response;
+            console.log('Success:', data);
+            return data;
+        } catch (error) {
+            console.error('Error uploading:', error);
+            return null;
+        }
+    }
+
+
+    async function cropPageRequest(file, pageNumber, width, height, x, y) {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('pageNumber', pageNumber);
+        formData.append('width', width);
+        formData.append('height', height);
+        formData.append('x', x);
+        formData.append('y', y);
+        
+
+        try {
+            const response = await fetch('https://localhost:7199/api/PDF/CropPage', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData?.message || `HTTP error! status: ${response.status}`;
+                throw new Error(errorMessage);
+            }
+
+            const data = await response;
+            console.log('Success:', data);
+            return data;
+        } catch (error) {
+            console.error('Error uploading:', error);
+            return null;
+        }
+    }
+
+
+    async function combinePdfFilesRequest(file, file2) {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('file2', file2);
+
+        try {
+            const response = await fetch('https://localhost:7199/api/PDF/CombinePdfFiles', {
                 method: 'POST',
                 body: formData
             });
